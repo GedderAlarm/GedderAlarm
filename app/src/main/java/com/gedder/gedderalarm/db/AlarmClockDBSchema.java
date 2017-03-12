adf9abde2b3d6b0b5690fe4723397d28ec61eb08
UmanShahzad <emidanrko564@hotmail.com>
2017-03-11 20:36:47 -0500

    Javadocs added for AlarmClockDBSchema

--- app/src/main/java/com/gedder/gedderalarm/db/AlarmClockDBSchema.java
+++ app/src/main/java/com/gedder/gedderalarm/db/AlarmClockDBSchema.java
@@ -7,32 +7,40 @@ package com.gedder.gedderalarm.db;
 
 
 /**
- *
+ * The schema of the application's alarm clock database.
  */
 public class AlarmClockDBSchema {
 
     /**
-     *
+     * Contains data for each alarm clock.
      */
     public static final class AlarmClockTable {
         public static final String TABLE_NAME = "alarmClocks";
 
         public static final class Columns {
-            public static final String ID = "alarmClocks.id";
-            public static final String ALARM_TIME = "alarmClocks.alarmTime";
-            public static final String ALARM_SET = "alarmClocks.alarmSet";
+            /** Incrementing ID */
+            public static final String ID = TABLE_NAME + ".id";
+
+            /** The time in milliseconds for the alarm since the epoch. */
+            public static final String ALARM_TIME = TABLE_NAME + ".alarmTime";
+
+            /** Whether the alarm is set or not. */
+            public static final String ALARM_SET = TABLE_NAME + ".alarmSet";
         }
     }
 
     /**
-     *
+     * Contains a UUID to ID mapping for each alarm clock.
      */
     public static final class UuidToIdTable {
         public static final String TABLE_NAME = "uuidToId";
 
         public static final class Columns {
-            public static final String UUID = "uuidToId.uuid";
-            public static final String ID = "uuidToId.id";
+            /** The UUID of some alarm clock. */
+            public static final String UUID = TABLE_NAME + ".uuid";
+
+            /** The ID associated with some UUID of an alarm clock. */
+            public static final String ID = TABLE_NAME + ".id";
         }
     }
 }
