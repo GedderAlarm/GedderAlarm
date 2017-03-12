17a8ed8c29663be43a9cc9ca316d8122ddcc0c53
UmanShahzad <emidanrko564@hotmail.com>
2017-03-11 20:32:31 -0500

    Completed implementation of Alarm Clocks SQLite helper

--- app/src/main/java/com/gedder/gedderalarm/db/AlarmClockDBHelper.java
+++ app/src/main/java/com/gedder/gedderalarm/db/AlarmClockDBHelper.java
@@ -14,7 +14,10 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import com.gedder.gedderalarm.AlarmClock;
 import com.gedder.gedderalarm.db.AlarmClockDBSchema.AlarmClockTable;
+import com.gedder.gedderalarm.db.AlarmClockDBSchema.UuidToIdTable;
+import com.gedder.gedderalarm.util.Log;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -39,22 +42,37 @@ public class AlarmClockDBHelper extends SQLiteOpenHelper {
 
     /**
      * Creates all tables for the database.
+     * @see com.gedder.gedderalarm.db.AlarmClockDBSchema
      * @param db The database we create tables for.
      */
     @Override
     public void onCreate(SQLiteDatabase db) {
         String CREATE_ALARM_CLOCK_TABLE =
                 "CREATE TABLE " + AlarmClockTable.TABLE_NAME + "(" +
-                        AlarmClockTable.Columns.ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                        AlarmClockTable.Columns.UUID + ", " +
-                        AlarmClockTable.Columns.ALARM_TIME + " INTEGER," +
-                        AlarmClockTable.Columns.ALARM_SET + " BOOLEAN" +
+                            AlarmClockTable.Columns.ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                            AlarmClockTable.Columns.ALARM_TIME + " INT8," +
+                            AlarmClockTable.Columns.ALARM_SET + " BOOLEAN" +
                         ")";
+
+        String CREATE_UUID_TO_ID_TABLE =
+                "CREATE TABLE " + UuidToIdTable.TABLE_NAME + "(" +
+                        UuidToIdTable.Columns.UUID + " TEXT PRIMARY KEY," +
+                        UuidToIdTable.Columns.ID + " INTEGER," +
+                        "FOREIGN KEY(" +
+                                UuidToIdTable.Columns.ID +
+                            ")" +
+                        "REFERENCES " + AlarmClockTable.TABLE_NAME + "(" +
+                                UuidToIdTable.Columns.ID +
+                            ")" +
+                        ")";
+
         db.execSQL(CREATE_ALARM_CLOCK_TABLE);
+        db.execSQL(CREATE_UUID_TO_ID_TABLE);
     }
 
     /**
      * Will drop all tables if they exist and recreate them.
+     * @see com.gedder.gedderalarm.db.AlarmClockDBSchema
      * @param db The database to drop and recreate.
      * @param oldVersion Old version of the database.
      * @param newVersion New version of the database.
@@ -62,85 +80,206 @@ public class AlarmClockDBHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         String DROP_ALARM_CLOCK_TABLE = "DROP TABLE IF EXISTS " + AlarmClockTable.TABLE_NAME;
+        String DROP_UUID_TO_ID_TABLE = "DROP TABLE IF EXISTS " + UuidToIdTable.TABLE_NAME;
         db.execSQL(DROP_ALARM_CLOCK_TABLE);
+        db.execSQL(DROP_UUID_TO_ID_TABLE);
     }
 
     /**
-     *
-     * @param alarmClock
+     * Adds an alarm clock to the database, including the time for its alarm, and whether it is
+     * currently set or not. Also adds its UUID to the database.
+     * @param alarmClock The AlarmClock object to add to the database.
      */
     public void addAlarmClock(AlarmClock alarmClock) {
         SQLiteDatabase db = this.getWritableDatabase();
 
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(AlarmClockTable.Columns.UUID, alarmClock.getUUID().toString());
-        contentValues.put(AlarmClockTable.Columns.ALARM_TIME, alarmClock.getAlarmTime());
-        contentValues.put(AlarmClockTable.Columns.ALARM_SET, alarmClock.isSet());
+        // Getting the next ID to map UUID to it in the uuidToID table.
+        int nextId = getLastId() + 1;
 
-        db.insert(AlarmClockTable.TABLE_NAME, null, contentValues);
+        // SQLite alarm clock insertion strings
+        String INSERT_ALARM_CLOCK =
+                "INSERT INTO " + AlarmClockTable.TABLE_NAME + "(" +
+                            AlarmClockTable.Columns.ALARM_TIME + "," +
+                            AlarmClockTable.Columns.ALARM_SET +
+                        ") VALUES (" +
+                            alarmClock.getAlarmTime() + ", " + alarmClock.isSet() +
+                        ")";
+
+        // SQLite UUID-ID mapping insertion strings
+        String INSERT_UUID_TO_ID =
+                "INSERT INTO " + UuidToIdTable.TABLE_NAME + "(" +
+                            UuidToIdTable.Columns.UUID + "," +
+                            UuidToIdTable.Columns.ID +
+                        ") VALUES (" +
+                            alarmClock.getUUID() + "," + nextId +
+                        ")";
+
+        db.execSQL(INSERT_ALARM_CLOCK);
+        db.execSQL(INSERT_UUID_TO_ID);
         db.close();
     }
 
     /**
-     *
-     * @param id
-     * @return
+     * Gets the alarm clock with UUID uuid, if it exists in the database.
+     * @param context The required context for the alarm clock.
+     * @param uuid The UUID of the alarm clock to get.
+     * @return The AlarmClock object having UUID uuid.
      */
-    public AlarmClock getAlarmClock(Context context, int id) {
+    public AlarmClock getAlarmClock(Context context, UUID uuid) {
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = db.query(AlarmClockTable.TABLE_NAME,
-                new String[] {
-                        AlarmClockTable.Columns.ID,
-                        AlarmClockTable.Columns.UUID,
-                        AlarmClockTable.Columns.ALARM_TIME,
-                        AlarmClockTable.Columns.ALARM_SET
-                },
-                AlarmClockTable.Columns.ID + "=?",
-                new String[] { String.valueOf(id)},
-                null, null, null, null);
+        Cursor cursor = db.rawQuery(
+                "SELECT " + AlarmClockTable.Columns.ALARM_TIME + "," +
+                            AlarmClockTable.Columns.ALARM_SET + " " +
+                "FROM " + AlarmClockTable.TABLE_NAME + "," + UuidToIdTable.TABLE_NAME + " " +
+                "WHERE " + AlarmClockTable.Columns.ID + "=" + UuidToIdTable.Columns.ID + " " +
+                "AND " + UuidToIdTable.Columns.UUID + "='" + uuid.toString() + "'",
+                null);
 
         if (cursor != null)
             cursor.moveToFirst();
 
-        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
-        UUID uuid = UUID.fromString(cursor.getString(1));
-        long scheduledAlarmTime = cursor.getLong(2);
-        boolean alarmSet = cursor.getInt(3) > 0;
-
-        return new AlarmClock(context, alarmManager, uuid, scheduledAlarmTime, alarmSet);
+        try {
+            AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+            long scheduledAlarmTime = cursor.getLong(0);
+            boolean alarmSet = cursor.getInt(1) > 0;
+            cursor.close();
+            return new AlarmClock(context, alarmManager, uuid, scheduledAlarmTime, alarmSet);
+        } catch (NullPointerException e) {
+            Log.e(TAG, "getAlarmClock() NullPointerException");
+            return new AlarmClock(context);
+        }
     }
 
     /**
-     *
-     * @return
+     * Returns a list of all alarm clocks currently in the database, active or not.
+     * @param context The required context for each alarm clock.
+     * @return A list of all alarm clocks currently existing in the database.
      */
-    public List<AlarmClock> getAllAlarmClocks() {
+    public List<AlarmClock> getAllAlarmClocks(Context context) {
+        List<AlarmClock> alarmClockList = new ArrayList<>();
+        String selectQuery = "SELECT  * FROM " + AlarmClockTable.TABLE_NAME;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(
+                "SELECT " + AlarmClockTable.Columns.ALARM_TIME + "," +
+                            AlarmClockTable.Columns.ALARM_SET + "," +
+                            UuidToIdTable.Columns.UUID +
+                "FROM " + AlarmClockTable.TABLE_NAME + "," + UuidToIdTable.TABLE_NAME + " " +
+                "WHERE " + AlarmClockTable.Columns.ID + "=" + UuidToIdTable.Columns.ID,
+                null);
 
+        if (cursor.moveToFirst()) {
+            AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+            do {
+                long scheduledAlarmTime = cursor.getLong(0);
+                boolean alarmSet = cursor.getInt(1) > 0;
+                UUID uuid = UUID.fromString(cursor.getString(2));
+
+                alarmClockList.add(new AlarmClock(context, alarmManager, uuid,
+                        scheduledAlarmTime, alarmSet));
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        return alarmClockList;
     }
 
     /**
-     *
-     * @return
+     * Returns the number of alarm clocks in the database, active or not.
+     * @return The number of alarm clocks in the database.
      */
     public int getAlarmClockCount() {
-
+        String countQuery = "SELECT  * FROM " + AlarmClockTable.TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.close();
+        return cursor.getCount();
     }
 
     /**
-     *
-     * @param alarmClock
-     * @return
+     * Updates an existing alarm clock to a new time. Assumes alarm is set.
+     * @see #updateAlarmClock(UUID, long, boolean)
+     * @param uuid The UUID of the alarm clock to update.
+     * @param scheduledTimeInMs The new scheduled time for the alarm clock.
+     * @return Number of rows affected by the update.
      */
-    public int updateAlarmClock(AlarmClock alarmClock) {
-
+    public int updateAlarmClock(UUID uuid, long scheduledTimeInMs) {
+        return updateAlarmClock(uuid, scheduledTimeInMs, true);
     }
 
     /**
-     *
-     * @param alarmClock
+     * Updates an existing alarm clock to a new time.
+     * @param uuid The UUID of the alarm clock to update.
+     * @param scheduledTimeInMs The new scheduled time for the alarm clock.
+     * @param alarmSet Whether this alarm is set or not.
+     * @return Number of rows affected by the update.
      */
-    public void deleteAlarmClock(AlarmClock alarmClock) {
+    public int updateAlarmClock(UUID uuid, long scheduledTimeInMs, boolean alarmSet) {
+        SQLiteDatabase db = this.getWritableDatabase();
 
+        // First get the ID by matching for it with UUID in one query.
+        // Then do the regular update using the convenience methods below.
+
+        // Getting the ID associated with the input UUID.
+        int id = getId(uuid);
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(AlarmClockTable.Columns.ALARM_TIME, scheduledTimeInMs);
+        contentValues.put(AlarmClockTable.Columns.ALARM_SET, alarmSet);
+
+        return db.update(AlarmClockTable.TABLE_NAME, contentValues,
+                AlarmClockTable.Columns.ID + "=?", new String[] { String.valueOf(id) });
+    }
+
+    /**
+     * Deletes the alarm clock that has UUID uuid.
+     * @param uuid The UUID of the alarm clock to delete.
+     */
+    public void deleteAlarmClock(UUID uuid) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        int id = getId(uuid);
+        db.delete(AlarmClockTable.TABLE_NAME,
+                AlarmClockTable.Columns.ID + "=?", new String[] { String.valueOf(id) });
+        db.close();
+    }
+
+    /**
+     * Gets the ID of the alarm clock with UUID uuid.
+     * @param uuid UUID of the alarm clock to get the ID of.
+     * @return The ID of the alarm clock associated with uuid. -1 if it doesn't exist.
+     */
+    private int getId(UUID uuid) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        int id = -1;
+        Cursor idCursor = db.rawQuery(
+                "SELECT " + UuidToIdTable.Columns.ID + " " +
+                "FROM " + UuidToIdTable.TABLE_NAME + " " +
+                "WHERE " + UuidToIdTable.Columns.UUID + "='" + uuid.toString() + "'",
+                null);
+        if (idCursor != null) {
+            idCursor.moveToFirst();
+            id = idCursor.getInt(0);
+        }
+        idCursor.close();
+        db.close();
+        return id;
+    }
+
+    /**
+     * Gets the last ID in the database.
+     * @return The last ID that exists in the database. -1 if no alarm clocks exist.
+     */
+    private int getLastId() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor idCursor = db.rawQuery("SELECT seq FROM sqlite_sequence " +
+                "WHERE NAME=\"" + AlarmClockTable.TABLE_NAME + "\"", null);
+        int lastId = -1;
+        if (idCursor != null) {
+            idCursor.moveToFirst();
+            lastId = idCursor.getInt(0);
+        }
+        idCursor.close();
+        db.close();
+        return lastId;
     }
 }
--- app/src/main/java/com/gedder/gedderalarm/db/AlarmClockDBSchema.java
+++ app/src/main/java/com/gedder/gedderalarm/db/AlarmClockDBSchema.java
@@ -6,15 +6,33 @@
 package com.gedder.gedderalarm.db;
 
 
+/**
+ *
+ */
 public class AlarmClockDBSchema {
+
+    /**
+     *
+     */
     public static final class AlarmClockTable {
         public static final String TABLE_NAME = "alarmClocks";
 
         public static final class Columns {
-            public static final String ID = "id";
-            public static final String UUID = "uuid";
-            public static final String ALARM_TIME = "alarmTime";
-            public static final String ALARM_SET = "alarmSet";
+            public static final String ID = "alarmClocks.id";
+            public static final String ALARM_TIME = "alarmClocks.alarmTime";
+            public static final String ALARM_SET = "alarmClocks.alarmSet";
+        }
+    }
+
+    /**
+     *
+     */
+    public static final class UuidToIdTable {
+        public static final String TABLE_NAME = "uuidToId";
+
+        public static final class Columns {
+            public static final String UUID = "uuidToId.uuid";
+            public static final String ID = "uuidToId.id";
         }
     }
 }
