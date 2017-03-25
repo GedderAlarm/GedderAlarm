89c989f1e183659c4562bf516dd1cb8f53f3e08f
UmanShahzad <emidanrko564@hotmail.com>
2017-03-25 17:44:31 -0400

    Rename EditAlarm to AlarmEdit; matches other styles.

--- app/src/main/java/com/gedder/gedderalarm/AlarmEditScrollingActivity.java
+++ app/src/main/java/com/gedder/gedderalarm/AlarmEditScrollingActivity.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class EditAlarmScrollingActivity extends AppCompatActivity {
+public class AlarmEditScrollingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_alarm_scrolling);
+        setContentView(R.layout.activity_alarm_edit_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
