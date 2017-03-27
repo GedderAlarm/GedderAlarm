9a2913673a621ce470d3d2f90fa5978c2cbd0a02
UmanShahzad <emidanrko564@hotmail.com>
2017-03-27 02:53:41 -0400

    Big SQLite optimizations.

--- app/src/main/java/com/gedder/gedderalarm/db/AlarmClockDBSchema.java
+++ app/src/main/java/com/gedder/gedderalarm/db/AlarmClockDBSchema.java
@@ -16,20 +16,9 @@ public class AlarmClockDBSchema {
 
         public static final class Columns {
             public static final String ID = "_id";
+            public static final String UUID = TABLE_NAME + "_uuid";
             public static final String ALARM_TIME = TABLE_NAME + "_alarmTime";
             public static final String ALARM_SET = TABLE_NAME + "_alarmSet";
-        }
-    }
-
-    /**
-     * Contains a UUID to ID mapping for each alarm clock.
-     */
-    public static final class UuidToIdTable {
-        public static final String TABLE_NAME = "uuidToId";
-
-        public static final class Columns {
-            public static final String UUID = TABLE_NAME + "_uuid";
-            public static final String ID = TABLE_NAME + "_id";
         }
     }
 }
