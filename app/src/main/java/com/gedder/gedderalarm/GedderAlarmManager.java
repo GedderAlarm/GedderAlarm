6e0d91ef738e7ccc01460078b6539fd6a43f968c
UmanShahzad <emidanrko564@hotmail.com>
2017-03-28 01:02:12 -0400

    Made all GedderAlarmManager methods static.

--- app/src/main/java/com/gedder/gedderalarm/GedderAlarmManager.java
+++ app/src/main/java/com/gedder/gedderalarm/GedderAlarmManager.java
@@ -26,89 +26,92 @@ public final class GedderAlarmManager {
 
     private GedderAlarmManager() {}
 
-    public void cancel(PendingIntent operation) {
+    public static void cancel(PendingIntent operation) {
         sAlarmManager.cancel(operation);
     }
 
     @TargetApi(24)
-    public void cancel(AlarmManager.OnAlarmListener listener) {
+    public static void cancel(AlarmManager.OnAlarmListener listener) {
         if (Build.VERSION.SDK_INT >= 24)
             sAlarmManager.cancel(listener);
     }
 
     @TargetApi(21)
-    public AlarmManager.AlarmClockInfo getNextAlarmClock() {
+    public static AlarmManager.AlarmClockInfo getNextAlarmClock() {
         if (Build.VERSION.SDK_INT >= 21)
             return sAlarmManager.getNextAlarmClock();
         return null;
     }
 
-    public void set(int type, long triggerAtMillis, PendingIntent operation) {
+    public static void set(int type, long triggerAtMillis, PendingIntent operation) {
         sAlarmManager.set(type, triggerAtMillis, operation);
     }
 
     @TargetApi(24)
-    public void set(int type, long triggerAtMillis, String tag,
+    public static void set(int type, long triggerAtMillis, String tag,
                     AlarmManager.OnAlarmListener listener, Handler targetHandler) {
         if (Build.VERSION.SDK_INT >= 24)
             sAlarmManager.set(type, triggerAtMillis, tag, listener, targetHandler);
     }
 
     @TargetApi(21)
-    public void setAlarmClock(AlarmManager.AlarmClockInfo info, PendingIntent operation) {
+    public static void setAlarmClock(AlarmManager.AlarmClockInfo info, PendingIntent operation) {
         if (Build.VERSION.SDK_INT >= 21)
             sAlarmManager.setAlarmClock(info, operation);
     }
 
     @TargetApi(23)
-    public void setAndAllowWhileIdle(int type, long triggerAtMillis, PendingIntent operation) {
+    public static void setAndAllowWhileIdle(int type, long triggerAtMillis,
+                                            PendingIntent operation) {
         if (Build.VERSION.SDK_INT >= 23)
             sAlarmManager.setAndAllowWhileIdle(type, triggerAtMillis, operation);
     }
 
-    public void setExact(int type, long triggerAtMillis, PendingIntent operation) {
+    public static void setExact(int type, long triggerAtMillis, PendingIntent operation) {
         sAlarmManager.setExact(type, triggerAtMillis, operation);
     }
 
     @TargetApi(24)
-    public void setExact(int type, long triggerAtMillis, String tag,
+    public static void setExact(int type, long triggerAtMillis, String tag,
                          AlarmManager.OnAlarmListener listener, Handler targetHandler) {
         if (Build.VERSION.SDK_INT >= 24)
             sAlarmManager.setExact(type, triggerAtMillis, tag, listener, targetHandler);
     }
 
     @TargetApi(23)
-    public void setExactAndAllowWhileIdle(int type, long triggerAtMillis, PendingIntent operation) {
+    public static void setExactAndAllowWhileIdle(int type, long triggerAtMillis,
+                                                 PendingIntent operation) {
         if (Build.VERSION.SDK_INT >= 23)
             sAlarmManager.setAndAllowWhileIdle(type, triggerAtMillis, operation);
     }
 
-    public void setInexactRepeating(int type, long triggerAtMillis, long intervalMillis,
+    public static void setInexactRepeating(int type, long triggerAtMillis, long intervalMillis,
                                     PendingIntent operation) {
         sAlarmManager.setInexactRepeating(type, triggerAtMillis, intervalMillis, operation);
     }
 
-    public void setRepeating(int type, long triggerAtMillis, long intervalMillis,
+    public static void setRepeating(int type, long triggerAtMillis, long intervalMillis,
                              PendingIntent operation) {
         sAlarmManager.setRepeating(type, triggerAtMillis, intervalMillis, operation);
     }
 
-    public void setTime(long millis) {
+    public static void setTime(long millis) {
         sAlarmManager.setTime(millis);
     }
 
-    public void setTimeZone(String timeZone) {
+    public static void setTimeZone(String timeZone) {
         sAlarmManager.setTimeZone(timeZone);
     }
 
-    public void setWindow(int type, long windowStartMillis, long windowLengthMillis,
+    public static void setWindow(int type, long windowStartMillis, long windowLengthMillis,
                           PendingIntent operation) {
         sAlarmManager.setWindow(type, windowStartMillis, windowLengthMillis, operation);
     }
 
     @TargetApi(24)
-    public void setWindow(int type, long windowStartMillis, long windowLengthMillis, String tag,
-                          AlarmManager.OnAlarmListener listener, Handler targetHandler) {
+    public static void setWindow(int type, long windowStartMillis, long windowLengthMillis,
+                                 String tag, AlarmManager.OnAlarmListener listener,
+                                 Handler targetHandler) {
         if (Build.VERSION.SDK_INT >= 24)
             sAlarmManager.setWindow(
                     type, windowStartMillis, windowLengthMillis, tag, listener, targetHandler);
