1171776cbdad48b0dcd3659f8a1a62f777753383
UmanShahzad <emidanrko564@hotmail.com>
2017-03-27 23:42:15 -0400

    Add empty GedderEngine class.

---
+++ app/src/main/java/com/gedder/gedderalarm/model/GedderEngine.java
@@ -0,0 +1,18 @@
+/*
+ * USER: mslm
+ * DATE: 3/27/17
+ */
+
+package com.gedder.gedderalarm.model;
+
+
+/**
+ *
+ */
+public final class GedderEngine {
+    private static final String TAG = GedderEngine.class.getSimpleName();
+
+    private GedderEngine() {}
+
+    
+}
