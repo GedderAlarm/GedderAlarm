c82bb6c187f13dd34230aa73eed94a652b4bf0dd
UmanShahzad <emidanrko564@hotmail.com>
2017-04-04 22:19:26 -0400

    Add TravelMode enum for URLGenerator/JsonParser.

---
+++ app/src/main/java/com/gedder/gedderalarm/google/TravelMode.java
@@ -0,0 +1,15 @@
+/*
+ * USER: mslm
+ * DATE: 4/4/17
+ */
+
+package com.gedder.gedderalarm.google;
+
+/**  */
+
+public enum TravelMode {
+    DRIVING,
+    WALKING,
+    BICYCLING,
+    TRANSIT,
+}
