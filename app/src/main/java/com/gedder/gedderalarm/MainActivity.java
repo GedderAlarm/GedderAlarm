f8ff21d9413a108d52ffebd7758306bb79ab22c2
UmanShahzad <emidanrko564@hotmail.com>
2017-04-28 13:35:04 -0400

    Gedder Alarm persistent icon shows on initial creation.

--- app/src/main/java/com/gedder/gedderalarm/MainActivity.java
+++ app/src/main/java/com/gedder/gedderalarm/MainActivity.java
@@ -12,7 +12,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CheckBox;
@@ -235,6 +234,7 @@ public class MainActivity extends AppCompatActivity {
 
                 if (alarmClock.isGedderEligible() && !alarmClock.isGedderOn()) {
                     alarmClock.turnGedderOn();
+                    setGedderPersistentIcon();
                 } else if (!alarmClock.isGedderEligible() && alarmClock.isGedderOn()) {
                     alarmClock.turnGedderOff();
                 }
